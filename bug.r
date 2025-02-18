d187d57d1cbbca1cb42e96c28d321abe1ac48596
BugHunter-X <askzenai@gmail.com>
2025-02-18 21:10:39 +0100

    Add bug.r file

---
+++ bug.r
@@ -0,0 +1,5 @@
+```r
+# This code attempts to subset a data frame based on a condition, but it uses incorrect syntax.
+data <- data.frame(a = 1:5, b = 6:10)
+subset <- data[data$a > 2 & data$b < 8, ]
+```
